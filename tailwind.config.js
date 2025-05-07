318bbb3934df9942ffb88a97f0a2e5fe8db77f8d
VirakbothSoth <virakbothsoth@gmail.com>
2025-05-07 23:17:38 +0700

    Added my main portfolio into the website.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        bg: '#f2f1f9',
+      }
+    },
+  },
+  plugins: [],
+};
+
