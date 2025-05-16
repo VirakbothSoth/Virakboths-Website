874c5dd1c6cb2445844281e5e8e24c8238a14e16
SamTheSanc <virakbothsoth@gmail.com>
2025-05-16 19:42:32 +0700

    Update Certs.jsx

--- src/components/Certs.jsx
+++ src/components/Certs.jsx
@@ -15,56 +15,56 @@ const Certs = () => {
   ];
 
   const webCerts = [
-    { image: fes, url: 'https://coursera.org/account/accomplishments/specialization/certificate/N1KVLB2O64O2', altText: 'Meta Front-End Specialization' },
-    { image: bes, url: 'https://coursera.org/account/accomplishments/specialization/certificate/W74F1I8L92RQ', altText: 'Meta Back-End Specialization' },
+    { image: fes, url: 'https://coursera.org/verify/specialization/N1KVLB2O64O2', altText: 'Meta Front-End Specialization' },
+    { image: bes, url: 'https://coursera.org/verify/specialization/W74F1I8L92RQ', altText: 'Meta Back-End Specialization' },
 
-    { image: f1, url: 'https://coursera.org/account/accomplishments/certificate/MMU2WMZEKWOA', altText: 'Meta Front-End Intro Certificate' },
-    { image: f2, url: 'https://coursera.org/account/accomplishments/certificate/LO2FMY48V1BX', altText: 'Meta JavaScript Certificate' },
-    { image: f3, url: 'https://coursera.org/account/accomplishments/certificate/0RAM91AXMKNF', altText: 'Meta HTML CSS Depth Certificate' },
-    { image: f4, url: 'https://coursera.org/account/accomplishments/certificate/SFY4SH8WP7J0', altText: 'Meta React Basics Certificate' },
-    { image: f5, url: 'https://coursera.org/account/accomplishments/certificate/Z5OHBQ707WJW', altText: 'Meta React Advanced Certificate' },
-    { image: f6, url: 'https://coursera.org/account/accomplishments/certificate/QNQLDDB6CYAT', altText: 'Meta UX UI Certificate' },
-    { image: f7, url: 'https://coursera.org/account/accomplishments/certificate/MGPD0EVNC2FY', altText: 'Meta Front-End Capstone Certificate' },
+    { image: f1, url: 'https://coursera.org/verify/MMU2WMZEKWOA', altText: 'Meta Front-End Intro Certificate' },
+    { image: f2, url: 'https://coursera.org/verify/LO2FMY48V1BX', altText: 'Meta JavaScript Certificate' },
+    { image: f3, url: 'https://coursera.org/verify/0RAM91AXMKNF', altText: 'Meta HTML CSS Depth Certificate' },
+    { image: f4, url: 'https://coursera.org/verify/SFY4SH8WP7J0', altText: 'Meta React Basics Certificate' },
+    { image: f5, url: 'https://coursera.org/verify/Z5OHBQ707WJW', altText: 'Meta React Advanced Certificate' },
+    { image: f6, url: 'https://coursera.org/verify/QNQLDDB6CYAT', altText: 'Meta UX UI Certificate' },
+    { image: f7, url: 'https://coursera.org/verify/MGPD0EVNC2FY', altText: 'Meta Front-End Capstone Certificate' },
 
-    { image: b1, url: 'https://coursera.org/account/accomplishments/certificate/ACCBKW4XQTWK', altText: 'Meta Back-End Intro Certificate' },
-    { image: b2, url: 'https://coursera.org/account/accomplishments/certificate/76NFVZC1XF3E', altText: 'Meta Python Certificate' },
-    { image: b3, url: 'https://coursera.org/account/accomplishments/certificate/QVJ03ZPZPJWJ', altText: 'Meta Intro to Databases Certificate' },
-    { image: b4, url: 'https://coursera.org/account/accomplishments/certificate/K638QW9RHVZT', altText: 'Meta Django Certificate' },
-    { image: b5, url: 'https://coursera.org/account/accomplishments/certificate/KEFAZVT628EP', altText: 'Meta API Certificate' },
-    { image: b6, url: 'https://coursera.org/account/accomplishments/certificate/T5QTGTRCRD35', altText: 'Meta Full Stack Certificate' },
-    { image: b7, url: 'https://coursera.org/account/accomplishments/certificate/QDB61GFOAD5W', altText: 'Meta Back-End Capstone Certificate' },
+    { image: b1, url: 'https://coursera.org/verify/ACCBKW4XQTWK', altText: 'Meta Back-End Intro Certificate' },
+    { image: b2, url: 'https://coursera.org/verify/76NFVZC1XF3E', altText: 'Meta Python Certificate' },
+    { image: b3, url: 'https://coursera.org/verify/QVJ03ZPZPJWJ', altText: 'Meta Intro to Databases Certificate' },
+    { image: b4, url: 'https://coursera.org/verify/K638QW9RHVZT', altText: 'Meta Django Certificate' },
+    { image: b5, url: 'https://coursera.org/verify/KEFAZVT628EP', altText: 'Meta API Certificate' },
+    { image: b6, url: 'https://coursera.org/verify/T5QTGTRCRD35', altText: 'Meta Full Stack Certificate' },
+    { image: b7, url: 'https://coursera.org/verify/QDB61GFOAD5W', altText: 'Meta Back-End Capstone Certificate' },
 
-    { image: iv, url: 'https://coursera.org/account/accomplishments/certificate/74FG463109SB', altText: 'Meta Coding Interview Certificate' },
-    { image: ve, url: 'https://coursera.org/account/accomplishments/certificate/4NSF48QD3FIS', altText: 'Meta Version Control Certificate' },
+    { image: iv, url: 'https://coursera.org/verify/74FG463109SB', altText: 'Meta Coding Interview Certificate' },
+    { image: ve, url: 'https://coursera.org/verify/4NSF48QD3FIS', altText: 'Meta Version Control Certificate' },
 
-    { image: ib_js, url: 'https://coursera.org/account/accomplishments/certificate/YFSJZECZWFAG', altText: 'IBM Javascript Essentials Certificate' },
-    { image: ib_be, url: 'https://coursera.org/account/accomplishments/certificate/S7O0PT89GJNY', altText: 'IBM Back-End Apps with Node & Express Certificate' },
+    { image: ib_js, url: 'https://coursera.org/verify/YFSJZECZWFAG', altText: 'IBM Javascript Essentials Certificate' },
+    { image: ib_be, url: 'https://coursera.org/verify/S7O0PT89GJNY', altText: 'IBM Back-End Apps with Node & Express Certificate' },
 
-    { image: jhu, url: 'https://coursera.org/account/accomplishments/certificate/8DILH4N0ABM6', altText: 'JHU Web Dev Certificate' },
+    { image: jhu, url: 'https://coursera.org/verify/8DILH4N0ABM6', altText: 'JHU Web Dev Certificate' },
 
     { image: rwd, url: 'https://freecodecamp.org/certification/virakboth_soth/responsive-web-design', altText: 'FreeCodeCamp Web Design Certificate' },
 
-    { image: awd, url: 'https://coursera.org/account/accomplishments/certificate/XD9F6TQH85E4', altText: 'Accessible Web Development Certificate' },
+    { image: awd, url: 'https://coursera.org/verify/XD9F6TQH85E4', altText: 'Accessible Web Development Certificate' },
   ];
 
   const otherCerts = [
     { image: cc, url: 'https://codecombat.com/certificates/6533825122ab2d0031d4ba79', altText: 'CodeCombat Certificate' },
-    { image: p1, url: 'https://coursera.org/account/accomplishments/certificate/NTMXA8WACBNQ', altText: 'UOM Python Intro Certificate' },
-    { image: p2, url: 'https://coursera.org/account/accomplishments/certificate/Z47RD2HY9PYR', altText: 'UOM Python Data Certificate' },
-    { image: p3, url: 'https://coursera.org/account/accomplishments/certificate/MTNK7QQL6XA3', altText: 'UOM Python Web Data Certificate' },
-    { image: hks, url: 'https://coursera.org/account/accomplishments/certificate/YHEPBKM5P9UU', altText: 'HKU Scratch Certificate' },
-    { image: hkse, url: 'https://coursera.org/account/accomplishments/certificate/GTPPJ35GF7JI', altText: 'HKU Software Engineer Certificate' },
-    { image: dc, url: 'https://coursera.org/account/accomplishments/certificate/4NSF48QD3FIS', altText: 'Cisco Digital Content Creation Certificate' },
+    { image: p1, url: 'https://coursera.org/verify/NTMXA8WACBNQ', altText: 'UOM Python Intro Certificate' },
+    { image: p2, url: 'https://coursera.org/verify/Z47RD2HY9PYR', altText: 'UOM Python Data Certificate' },
+    { image: p3, url: 'https://coursera.org/verify/MTNK7QQL6XA3', altText: 'UOM Python Web Data Certificate' },
+    { image: hks, url: 'https://coursera.org/verify/YHEPBKM5P9UU', altText: 'HKU Scratch Certificate' },
+    { image: hkse, url: 'https://coursera.org/verify/GTPPJ35GF7JI', altText: 'HKU Software Engineer Certificate' },
+    { image: dc, url: 'https://coursera.org/verify/4NSF48QD3FIS', altText: 'Cisco Digital Content Creation Certificate' },
     { image: ud, url: 'https://netacad.com/certificates?issuanceId=b91dac97-f5d9-4ea6-acab-9397fb44401b', altText: 'Cisco Using Devices Certificate' },
     { image: scp, url: 'https://netacad.com/certificates?issuanceId=b91dac97-f5d9-4ea6-acab-9397fb44401b', altText: 'Cisco Using Devices Certificate' },
   ];
 
   const aiCerts = [
-    { image: s5, url: 'https://coursera.org/account/accomplishments/certificate/78PBQYZS06B9', altText: 'Scrimba AI for Web Development Specialization' },
-    { image: s1, url: 'https://coursera.org/account/accomplishments/certificate/RCW3SNB46E1N', altText: 'Scrimba Learn to Code with AI Certificate' },
-    { image: s2, url: 'https://coursera.org/account/accomplishments/certificate/F304YJ6YFM2X', altText: 'Scrimba Web Dev with AI Certificate' },
-    { image: s3, url: 'https://coursera.org/account/accomplishments/certificate/ZDSBCX0EHPNG', altText: 'Scrimba AI Engineering Intro Certificate' },
-    { image: s4, url: 'https://coursera.org/account/accomplishments/certificate/6YOVVKETFY5F', altText: 'Scrimba Deploy AI Apps with Cloudflare Certificate' },
+    { image: s5, url: 'https://coursera.org/verify/specialization/78PBQYZS06B9', altText: 'Scrimba AI for Web Development Specialization' },
+    { image: s1, url: 'https://coursera.org/verify/RCW3SNB46E1N', altText: 'Scrimba Learn to Code with AI Certificate' },
+    { image: s2, url: 'https://coursera.org/verify/F304YJ6YFM2X', altText: 'Scrimba Web Dev with AI Certificate' },
+    { image: s3, url: 'https://coursera.org/verify/ZDSBCX0EHPNG', altText: 'Scrimba AI Engineering Intro Certificate' },
+    { image: s4, url: 'https://coursera.org/verify/6YOVVKETFY5F', altText: 'Scrimba Deploy AI Apps with Cloudflare Certificate' },
   ];
 
   // Render certificates based on active category
